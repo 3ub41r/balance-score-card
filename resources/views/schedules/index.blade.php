7cccb7b14030157ffd39029f859a19166a39b8cd
zubair <zubair@utmspace.edu.my>
2022-04-13 09:43:02 +0800

    Add layout

--- resources/views/schedules/index.blade.php
+++ resources/views/schedules/index.blade.php
@@ -1,16 +1,4 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <title>Schedules!</title>
-</head>
-<body>
+<x-layout>
     <div class="container">
         <h1 class="my-3">Schedules!</h1>
 
@@ -139,7 +127,7 @@
             <button class="btn btn-primary">Save</button>
         </form>
     </div>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script>
         const yearSelect = document.querySelector('#year-select');
@@ -150,5 +138,4 @@
             window.location.replace("{{ route('schedules.index', ':year') }}".replace(':year', year));
         });
     </script>
-</body>
-</html>
+</x-layout>
